4da5c68469efff53ccfc7058627dd5ae18103eb4
Royweru <royezi17@gmail.com>
2024-03-20 22:02:16 +0300

    modified:   components/footer/contact.tsx

--- components/footer/contact.tsx
+++ components/footer/contact.tsx
@@ -1,22 +1,22 @@
-import { Facebook, LinkedinIcon } from 'lucide-react'
+import {  LinkedinIcon } from 'lucide-react'
 import React from 'react'
-import { BsFacebook, BsGithub, BsInstagram, BsTwitter, BsWhatsapp } from 'react-icons/bs'
+import {  BsGithub, BsInstagram, BsTwitter } from 'react-icons/bs'
 
 export const Contact = () => {
   return (
     <div className=' col-span-2 rounded-md p-2 flex justify-center gap-x-4 items-center' id='contact'>
       <a href="https://github.com/Royweru" target='_blank'>
-      <BsGithub className=' w-7 h-7'/>
+      <BsGithub className=' w-10 h-10'/>
       </a>
       
       <a href="https://twitter.com/roy_weru" target='_blank'>
-      <BsTwitter className=' w-7 h-7'/>
+      <BsTwitter className=' w-10 h-10'/>
       </a>
       <a href="https://www.instagram.com/codewer0/">
-      <BsInstagram className=' w-7 h-7' />
+      <BsInstagram className=' w-10 h-10' />
       </a>
       <a href="https://www.linkedin.com/in/roy-matheri-59b8a5245/" target='_blank'>
-      <LinkedinIcon className=' w-7 h-7' />
+      <LinkedinIcon className=' w-10 h-10' />
       </a>
     
     </div>
